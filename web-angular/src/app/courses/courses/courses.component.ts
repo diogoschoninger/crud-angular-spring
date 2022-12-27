e1f93fa99de4b3125df703575bd3639b87c9a205
Diogo Schoninger <schoninger.diogo@gmail.com>
2022-12-27 11:00:13 -0300

    Creating the course listing table

--- web-angular/src/app/courses/courses/courses.component.ts
+++ web-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
-
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-end' },
+    { _id: '2', name: 'Express', category: 'Back-end' },
+    { _id: '3', name: 'React', category: 'Front-end' },
+    { _id: '4', name: 'MySQL', category: 'Bancos de dados' },
+  ];
+  displayedColumns = ['name', 'category']
 }
