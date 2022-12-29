f8fef4d227e7fa88f0fce4382bfc71eed00cbb0d
Diogo Schoninger <schoninger.diogo@gmail.com>
2022-12-29 14:22:01 -0300

    Added loading spinner

--- web-angular/src/app/courses/services/courses.service.ts
+++ web-angular/src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { first, tap } from 'rxjs';
+import { first, delay } from 'rxjs';
 
 import { Course } from '../model/course';
 
@@ -16,7 +16,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
       .pipe(
         first(),
-        tap(courses => console.log(courses))
+        delay(1000)
       );
   }
 }
